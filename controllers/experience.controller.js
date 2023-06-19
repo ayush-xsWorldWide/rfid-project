b076dc16671ce85b5c85193cc6c7bce47373135b
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-06-19 10:12:59 +0530

    exp controller fixed

--- controllers/experience.controller.js
+++ controllers/experience.controller.js
@@ -1,19 +1,21 @@
 const Visiter = require('../model/visiter');
 
-exports.getExp = async(req, res)=>{
+exports.getExp = async (req, res) => {
     var { expid, cardno } = req.params;
-    try{
+    try {
         expid--; // storing data in a list where the index starts from 0
-        var data = await Visiter.findOne({cardno});
-        
-        if(data.exp[expid] === "No") data.exp[expid] = "Yes";
-        else data.exp[expid] = "No";
-        
-        data.save();
-        return res.send("Response received!");
+        var data = await Visiter.findOne({ cardno });
+
+        if (data.status === "ACTIVE") {
+            if (data.exp[expid] === "No") data.exp[expid] = "Yes";
+            else data.exp[expid] = "No";
+            data.save();
+            return res.send("Response received!");
+        }
+        return res.send("Inactive card!");
     }
-    catch(error){
+    catch (error) {
         console.log("Some error occured!");
-        return res.status(500).send("Server Error");       
+        return res.status(500).send("Server Error");
     }
 }
